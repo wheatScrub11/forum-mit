63c0ee3e9fac87bc303ccd64d2546d9fee202daa
Zawent <fabianvargas1099@gmail.com>
2025-03-05 16:15:15 -0500

    sda

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,23 @@ import Post from './assets/Post'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
-  const [dataFromAppInventor, setDataFromAppInventor] = useState('vacio');
+  // const [dataFromAppInventor, setDataFromAppInventor] = useState('vacio');
 
-  useEffect(() => {
-    // Read data from local storage
-    const data = localStorage.getItem('appInventorData');
-    if (data) {
-      setDataFromAppInventor(data);
-      
-    }
+  // useEffect(() => {
+  //   // Read data from local storage
+  //   const data = localStorage.getItem('appInventorData');
+  //   if (data) {
+  //     setDataFromAppInventor(data);
+  //   }
 
-  }, []);
+  // }, []);
 
   return (
     <div id='MAIN-CONTAINER'>
       <div className="main">
-        <div>hola {dataFromAppInventor}</div>
+        {/* <div>hola {dataFromAppInventor}</div> */}
         <Post />
         <Post />
       </div>
